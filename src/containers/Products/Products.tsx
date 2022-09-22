37fa2895a9ecbab75261e414f80cff4f6bf30728
Farrukh Ameen <engr.farrukhameen@gmail.com>
2022-09-22 23:27:48 +0500

    cleanup

--- src/containers/Products/Products.tsx
+++ src/containers/Products/Products.tsx
@@ -9,22 +9,33 @@ import {useFilter} from '../../common/hooks/useFilter';
 function Products() {
   const {cart} = useCart();
   const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [filter, colour, setColour] = useFilter();
 
   useEffect(() => {
     fetchProducts()
-      .then((products) => setProducts(products))
-      .catch((error) => console.log('error', error));
+      .then((products) => {
+        setIsLoading(false);
+        setProducts(products);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log('error', error);
+      });
   }, []);
 
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white dark:bg-gray-900 min-h-[100vh]">
       <div className="px-6 py-10">
         <FilterBar colour={colour} setColour={setColour} />
         <div className="grid grid-cols-1 gap-8 mt-8 md:mt-16">
-          {products.filter(filter).map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
+          {isLoading ? (
+            <h1 className="text-5xl dark:text-white">Loading</h1>
+          ) : (
+            products
+              .filter(filter)
+              .map((product) => <Product key={product.id} product={product} />)
+          )}
           <div className="flex">
             <div className="flex relative flex-1"></div>
             <div className="flex flex-1 dark:text-white justify-center items-center">
